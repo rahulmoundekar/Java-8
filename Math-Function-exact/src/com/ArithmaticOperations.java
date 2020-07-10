4caf58b2822e848ec63a45b20c6f614872df2b3c
rahul <rahulmoundekar44@gmail.com>
2020-07-10 19:01:35 +0530

    Java 8 Maths class for Arithmetic Operations

---
+++ Math-Function-exact/src/com/ArithmaticOperations.java
@@ -0,0 +1,13 @@
+package com;
+
+public class ArithmaticOperations {
+	public static void main(String[] args) {
+
+		System.out.println(Math.addExact(10, 20));
+		System.out.println(Math.subtractExact(10, 20));
+		System.out.println(Math.multiplyExact(10, 20));
+		System.out.println(Math.floorMod(10, 20));
+
+	}
+
+}
